216c305331c992c0301739daebda5d14f7bfbc00
André Robert <fxxdev@outlook.com>
2019-08-08 11:12:20 -0300

    Laravel argon files upload with register and login ready

---
+++ resources/views/layouts/footers/nav.blade.php
@@ -0,0 +1,8 @@
+<div class="row align-items-center justify-content-xl-between">
+    <div class="col text-center pull-center align-items-center">
+        <div class="copyright text-center text-muted">
+            &copy; {{ now()->year }} <a href="https://github.com/FXXPSDEV" class="font-weight-bold ml-1" target="_blank">FXXPS Dev</a> 
+        </div>
+    </div>
+    
+</div>
