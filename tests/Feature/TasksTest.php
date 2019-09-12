106522ebf50704319128ac0a06a7acefb301ba11
André Robert <fxxdev@outlook.com>
2019-09-12 11:11:29 -0300

    user tests

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -17,4 +17,16 @@ class TaskTest extends TestCase
 
         $response->assertSee($task->title);
     }
+
+
+    public function a_user_can_read_single_task()
+    {
+        //Given we have task in the database
+        $task = factory('App\Task')->create();
+        //When user visit the task's URI
+        $response = $this->get('/tasks/'.$task->id);
+        //He can see the task details
+        $response->assertSee($task->title)
+            ->assertSee($task->description);
+    }
 }
